7986b7cc464d9b13324f963c5adcd5fed56379d2
emanzelekha <emanzelekha@emanzelekhas-Mac.local>
2016-12-29 11:41:27 -0800

    Initial Commit

---
+++ CustomButton/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CustomButton
+//
+//  Created by emanzelekha on 12/29/16.
+//  Copyright © 2016 emanzelekha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
